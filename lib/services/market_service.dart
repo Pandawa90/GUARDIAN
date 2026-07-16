0997e5092e86b0953ffdf8cdc09f4ed5242d7663
Pandawa90 <fiqichandra90@gmail.com>
2026-07-16 13:02:20 +0800

    Connect Twelve Data realtime API

--- lib/services/market_service.dart
+++ lib/services/market_service.dart
@@ -1,12 +1,27 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+
+import '../config.dart';
 import '../models/market_price.dart';
 
 class MarketService {
   Future<MarketPrice> getGoldPrice() async {
-    return MarketPrice(
-      symbol: "XAU/USD",
-      price: 3378.25,
-      trend: "Bullish",
-      lastUpdate: DateTime.now(),
-    );
+    final response = await http.get(Uri.parse(Config.baseUrl));
+
+    if (response.statusCode == 200) {
+      final data = jsonDecode(response.body);
+
+      final double price =
+          double.tryParse(data["price"].toString()) ?? 0.0;
+
+      return MarketPrice(
+        symbol: "XAU/USD",
+        price: price,
+        trend: "Live",
+        lastUpdate: DateTime.now(),
+      );
+    }
+
+    throw Exception("Gagal mengambil data dari Twelve Data");
   }
 }
