859146587b9733e4b50f673501ff78d6cd0dec5c
Pandawa90 <fiqichandra90@gmail.com>
2026-07-16 13:51:43 +0800

    Fix Twelve Data commodity endpoint

--- lib/config.dart
+++ lib/config.dart
@@ -2,5 +2,5 @@ class Config {
   static const String apiKey = "45851b5b15ad43d4be2ad22b62e65a54";
 
   static const String baseUrl =
-      "https://api.twelvedata.com/price?symbol=XAU/USD&apikey=$apiKey";
+      "https://api.twelvedata.com/price?symbol=XAU/USD&exchange=COMMODITY&apikey=$apiKey";
 }
